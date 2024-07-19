7395f6efb44ebcd9875a5c5eef75e11f387cc429
Quentin B <quentin@galadrim.fr>
2024-07-19 18:37:54 +0200

    feat: backend get word working

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,33 @@
+import "dotenv/config";
 import { PrismaClient } from "@prisma/client";
-import express from "express";
-
+import { Translator } from "deepl-node";
+import express, { Request, Response } from "express";
+import { Word } from "@madbox-assignment/types";
 const prisma = new PrismaClient();
 const app = express();
+
+const translator = new Translator(process.env.DEEPL_API_KEY);
 
 app.get("/api", (req, res) => {
   res.send({ message: "Welcome to backend!" });
 });
 
-app.get("/api/db-test", async (req, res) => {
-  const data = await prisma.verb.findFirst({ where: { id: 1 } });
-  res.send({ data });
+app.get("/api/word", async (req: Request, res: Response<Word>) => {
+  const verbTableLength = await prisma.verb.count();
+  const randomId = Math.floor(Math.random() * verbTableLength) + 1;
+  const { id, verb } = await prisma.verb.findFirst({
+    where: { id: randomId },
+    select: { id: true, verb: true },
+  });
+
+  const { text } = await translator.translateText(verb, null, "en-GB");
+  res.send({
+    id,
+    french: verb,
+    english: text,
+    length: text.length,
+    firstLetter: text[0],
+  });
 });
 
 const port = process.env.PORT || 3333;
---
+++ types/word.d.ts
@@ -0,0 +1,7 @@
+export type Word = {
+  id: number;
+  french: string;
+  english: string;
+  length: number;
+  firstLetter: string;
+};
